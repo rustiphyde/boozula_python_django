35a45e2b6707ff0527e11189e51ae0cf25ae60a7
rustiphyde <rustiphyde@gmail.com>
2019-09-08 20:00:14 -0500

    Add app config to settings.py INSTALLED APPS

--- boozula_python_django/settings.py
+++ boozula_python_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'booz_blog.apps.BoozBlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
