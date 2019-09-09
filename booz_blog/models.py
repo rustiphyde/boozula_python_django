7f239df4d1037fa9463d46b98f60324992186b25
rustiphyde <rustiphyde@gmail.com>
2019-09-08 23:10:29 -0500

    Utilize ORM to create Post model SQL

--- booz_blog/models.py
+++ booz_blog/models.py
@@ -1,6 +1,8 @@
+# ORM - Object Relational Mapper that creates SQL tables behind the scenes
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
