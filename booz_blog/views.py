d28e987639efefc48021fd4df0f0670e7f12fd6b
rustiphyde <rustiphyde@gmail.com>
2019-09-08 20:49:42 -0500

    Utilize django shortcut render function to show views

--- booz_blog/views.py
+++ booz_blog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+#fake post dictionaries
+posts = [
+    {
+        'author': 'Rustiphyde',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'September 8, 2019'
+    },
+    {
+        'author': 'Razorworm',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 8, 2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Booz-Blog Home<h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'booz_blog/home.html', context)
 def about(request):
-    return HttpResponse('<h1>Booz-Blog About<h1>')
+    return render(request, 'booz_blog/about.html')
+
+#default Django templates directory structure
+#app_name -> templates -> app_name -> templateName.html
+
