ad9814ebc93b966e4c8e8de67a0a915b9229b1ac
rustiphyde <rustiphyde@gmail.com>
2019-09-08 23:04:12 -0500

    Update migrations with Create model Post

--- booz_blog/models.py
+++ booz_blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Post(models.model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.foreignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
