e009c6d50a473e1c3260c235c70c7131c10c7dae
rustiphyde <rustiphyde@gmail.com>
2019-09-10 08:21:53 -0500

    Add ability to view profile picture in browser on profile view

--- boozula_python_django/urls.py
+++ boozula_python_django/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from users import views as user_views
 
@@ -27,3 +29,6 @@ urlpatterns = [
     path('', include('booz_blog.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
