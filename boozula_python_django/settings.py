f1fc9c0e18ce777f6750f02aeacc3f058d8a756b
rustiphyde <rustiphyde@gmail.com>
2019-09-11 16:16:22 -0500

    Update allowed hosts

--- boozula_python_django/settings.py
+++ boozula_python_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boozula.herokuapp.com']
+ALLOWED_HOSTS = ['boozula.herokuapp.com', 'localhost:8000', 'boozula.netlify.com']
 
 
 # Application definition
