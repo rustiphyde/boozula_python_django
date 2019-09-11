7915a250109e345494bac5ddd0135e1ea6b7a0bd
rustiphyde <rustiphyde@gmail.com>
2019-09-11 16:11:57 -0500

    Update settings

--- booz_blog/models.py
+++ booz_blog/models.py
@@ -21,15 +21,15 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    def save(self):
-        super().save()
+    # def save(self):
+    #     super().save()
         
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
         
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
             
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
