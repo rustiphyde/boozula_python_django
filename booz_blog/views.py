4a92b0bff6b7950c843843fcb11d19d89966cdcf
rustiphyde <rustiphyde@gmail.com>
2019-09-10 20:56:53 -0500

    define function to update boozula pics on posts

--- booz_blog/views.py
+++ booz_blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PicUpdateForm
+from django.contrib import messages
 
 def home(request):
     context = {
@@ -10,6 +12,22 @@ def home(request):
 def about(request):
     return render(request, 'booz_blog/about.html', {'title': 'About'})
 
-#default Django templates directory structure
-#app_name -> templates -> app_name -> templateName.html
+def poster(request):
+    if request.method == 'POST':
+        pic_form = PicUpdateForm(request.POST, 
+                                 request.FILES, 
+                                 instance=request.post)
+        if pic_form.is_valid():
+            pic_form.save()
+            messages.success(request, f'Your post has been updated!')
+            return redirect('blog-home')
+            
+    else:
+        pic_form = PicUpdateForm(instance=request.post)
+    
+    context = {
+        'pic_form': pic_form
+    }
+    return render(request, 'booz_blog/home.html', context)
+
 
