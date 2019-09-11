df8733f0c6fe7bca63eb0623f8e6adc59f842801
rustiphyde <rustiphyde@gmail.com>
2019-09-10 22:03:04 -0500

    Create form for creating a new post on the front end

--- booz_blog/views.py
+++ booz_blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PicUpdateForm
 from django.contrib import messages
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 
 def home(request):
     context = {
@@ -20,25 +23,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['image', 'title', 'main_alcohol', 'ingredients', 'preparation']
+    
 def about(request):
     return render(request, 'booz_blog/about.html', {'title': 'About'})
-
-def poster(request):
-    if request.method == 'POST':
-        pic_form = PicUpdateForm(request.POST, 
-                                 request.FILES, 
-                                 instance=request.post)
-        if pic_form.is_valid():
-            pic_form.save()
-            messages.success(request, f'Your post has been updated!')
-            return redirect('blog-home')
-            
-    else:
-        pic_form = PicUpdateForm(instance=request.post)
-    
-    context = {
-        'pic_form': pic_form
-    }
-    return render(request, 'booz_blog/home.html', context)
-
-
