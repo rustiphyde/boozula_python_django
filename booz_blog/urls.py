1d8333e35176a417f98c6b8a26eeb4d4ee5b9ee9
rustiphyde <rustiphyde@gmail.com>
2019-09-10 22:31:12 -0500

    Add Update post functionality and form

--- booz_blog/urls.py
+++ booz_blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     PostListView, 
     PostDetailView, 
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 from . import views
 
 urlpatterns = [
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('', PostListView.as_view(), name='blog-home'),
--- booz_blog/views.py
+++ booz_blog/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView
+    CreateView,
+    UpdateView
 )
 
 def home(request):
@@ -25,6 +26,14 @@ class PostDetailView(DetailView):
     model = Post
     
 class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['image', 'title', 'main_alcohol', 'ingredients', 'preparation']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['image', 'title', 'main_alcohol', 'ingredients', 'preparation']
     
