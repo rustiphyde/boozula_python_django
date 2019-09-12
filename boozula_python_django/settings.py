d777f4039b817fc2f9da9b6c12132f322ae44511
rustiphyde <rustiphyde@gmail.com>
2019-09-11 19:06:52 -0500

    Update settings

--- boozula_python_django/settings.py
+++ boozula_python_django/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'storages'
 ]
 
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
